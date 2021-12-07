ad8cc992d73ff8eb78cd665018193999e99128af
Alvaro <alvaromolinag97@gmail.com>
2021-12-07 09:55:49 +0100

    minas arregladas

--- SmashRobotsProject/Assets/Scripts/Mine.cs
+++ SmashRobotsProject/Assets/Scripts/Mine.cs
@@ -20,7 +20,8 @@ public class Mine : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-
+        Vector3 rot = transform.rotation.eulerAngles; 
+        transform.rotation = Quaternion.Euler(0, rot.y, 0);
     }
 
     // Update is called once per frame
